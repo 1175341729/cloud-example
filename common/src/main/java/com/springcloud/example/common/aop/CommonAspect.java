70f0f4d923273fdf7782358b4516e7a97a612c0d
dengwei <1175341729@qq.com>
2018-09-13 13:44:28 +0800

    切面

---
+++ common/src/main/java/com/springcloud/example/common/aop/CommonAspect.java
@@ -0,0 +1,38 @@
+package com.springcloud.example.common.aop;
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+/***
+ *  @Author dengwei
+ *  @Description: TODO
+ *  @Date 2018/9/13 13:35
+ */
+@Aspect
+@Component
+@Slf4j
+public class CommonAspect {
+    @Pointcut(value = "execution(* com.springcloud.example.*.controller.*.*(..))")
+    public void point(){
+
+    }
+
+    @Around(value = "point()")
+    public Object arround(ProceedingJoinPoint joinPoint){
+        String method = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+        log.info("请求方法：{}",method);
+        log.info("请求参数：{}", JSON.toJSONString(args));
+        try {
+            return joinPoint.proceed();
+        } catch (Throwable throwable) {
+            log.error("error",throwable);
+            return null;
+        }
+    }
+}
