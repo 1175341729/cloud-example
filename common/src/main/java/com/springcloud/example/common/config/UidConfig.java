d5875cdfc20c34d10c1a542b97e235cfe8f1cc1a
dengwei <1175341729@qq.com>
2019-02-18 13:52:48 +0800

    ADD

---
+++ common/src/main/java/com/springcloud/example/common/config/UidConfig.java
@@ -0,0 +1,14 @@
+package com.springcloud.example.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+/***
+ *  @Author dengwei
+ *  @Description: TODO
+ *  @Date 2019/2/15 17:28
+ */
+@Configuration
+//@ImportResource(locations = { "classpath:cached-uid-spring.xml" })
+public class UidConfig {
+}
